1dfaef9e9315fb5d152f3f6b52121cc825369ec5
Michael Eggert <meggert@uchicago.edu>
2020-01-16 16:17:28 -0600

    First test of blank paper being blank

---
+++ tests/TestPaper.py
@@ -0,0 +1,11 @@
+import unittest
+from pencil_durability import Paper
+
+class TestPaper(unittest.TestCase):
+    def test_that_initializing_blank_paper_is_blank_paper(self):
+        paper = Paper()
+        self.assertEqual('', paper.display_page())
+
+
+if __name__ == '__main__':
+    unittest.main()
